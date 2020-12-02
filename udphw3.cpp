394eb17e113c80630b0c56efbc4524e9e19f2e09
Jayden Stipek <55769079+Clause828@users.noreply.github.com>
2020-12-02 13:28:05 -0800

    updated clientStopWait to return resubmissions

--- udphw3.cpp
+++ udphw3.cpp
@@ -9,34 +9,45 @@ using namespace std;
 
 int clientStopWait( UdpSocket &sock, const int max, int message[] )
 {
-    "sends message[] and receives an acknowledgment from the server max";
-    "If the client cannot receive an acknowledgement immedietly,"; 
-    "it should start a Timer. If a timeout occurs (i.e., no response after 1500 usec), ";
-    "the client must resend the same message. The function must count the number of messages retransmitted and";
-    "return it to the main function as its return value.";
+    /**
+     * sends message[] and receives an acknowledgment from the server max
+     * If the client cannot receive an acknowledgement immedietly
+     * it should start a Timer. If a timeout occurs (i.e., no response after 1500 usec)
+     * the client must resend the same message. The function must count the number of messages retransmitted and
+     * return it to the main function as its return value.
+     **/
+
+    int resubmissions = 0;
     for(int i = 0; i < max; i++){ // loop for the 20000 times
         message[0] = i; //insert the message into message[0]
         Timer timer;
         bool response = false;
         int sent;
-        int resubmissions = 0;
+        sent = sock.sendTo((char*) message, MSGSIZE); //send the message to the server
         timer.start();
-        while(!response) //while I have not receieved an response
+        while(true) //while I have not receieved an response
         {
-            sent = sock.sendTo((char*) message, MSGSIZE); //send the message to the server
-            if(sent)
-                response = true; //response is true and then cut the while loop
+            int responseData = sock.pollRecvFrom(); //Any data been recieved?
+            if(responseData > 0)
+                break; //response is true and then cut the while loop
 
             if(timer.getUsec() >= TIMEOUT) //If we go over the 1500 Timeout 
             {
                 timer.lap(); //restart the timer    
-                sock.sendTo((char*) message, MSGSIZE); //resend the messages
                 resubmissions++; //add to resubmissions count
                 i--; //take away from the for loop total
+                break;
             }
         }
+        sock.recvFrom((char*) message, MSGSIZE); //receieve the message from ther server
+        if(message[0] != i) //If it is a bad message
+        {
+            i--;    
+            resubmissions++;
+            continue;
+        }
     }
-    return -1;
+    return resubmissions;
 }
 void serverReliable( UdpSocket &sock, const int max, int message[] )
 {
