2bc94952eae9a61a74cbb00c6667b1b353f252c5
Jayden Stipek <55769079+Clause828@users.noreply.github.com>
2020-12-04 22:39:01 -0800

    Update udphw3case4.cpp
    Now with Droprate initlization

--- udphw3case4.cpp
+++ udphw3case4.cpp
@@ -5,14 +5,17 @@
 #include "udphw3.h"
 
 #define TIMEOUT 1500
-
+#define DROPRATE 1
 using namespace std;
 
 
 /**
- * receives message[] and sends an acknowledgment to the client max (=20,000) times using the sock object. 
- * Every time the server receives a new message[], it must save the message's sequence number in an array and return a cumulative acknowledgment, 
- * i.e.,* the last received message in order.
+ *Modify hw3case4.cpp to include test case 4. Test case 4 runs similar to test case 3, 
+ but the "sliding window" algorithm/implementation only runs for a sliding window of size 1 and size 30. 
+ Add code to the serverEarlyRetrans function in udphw3case4.cpp so that packets are randomly dropped packets N% of the time, 
+ where N is every integer from 0 to 10. You will also need to modify the output of hw3case4.cpp so that it outputs the drop percentage 
+ (instead of the window size) into the file.
+  You can simulate a drop by just not returning an ACK when you receive a packet.
  * 
  **/
 void serverEarlyRetrans( UdpSocket &sock, const int max, int message[], int windowSize )
@@ -26,6 +29,9 @@ void serverEarlyRetrans( UdpSocket &sock, const int max, int message[], int wind
         {
             //cout << "message recieved" << endl;
             sock.recvFrom((char*) message, MSGSIZE)  ; //recieve the information
+            int percentage = rand() % 10;
+            if(percentage < DROPRATE)   //MODIFICATion for case 4 Drops 10% of the time
+                continue;
             //cout << "message: " << message[0] << " count: "  << count << endl;
             if(message[0] == count){
                 //cout << "acknolegment sent" << endl; //never gets in here
